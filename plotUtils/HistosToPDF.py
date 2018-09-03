b143ede6c2f80fae7a71a10fb86378de7cfd587f
Matthew Solt <mrsolt@stanford.edu>
2018-09-03 14:26:30 -0700

    add script to merge histos

---
+++ plotUtils/HistosToPDF.py
@@ -0,0 +1,71 @@
+import sys
+tmpargv = sys.argv
+sys.argv = []
+import getopt
+import ROOT
+from ROOT import gROOT, TFile, gDirectory, gStyle, TCanvas, TH1
+sys.argv = tmpargv
+
+#List arguments
+def print_usage():
+    print "\nUsage: {0} <output file base name> <input file name>".format(sys.argv[0])
+    print "Arguments: "
+    print '\t-h: this help message'
+    print
+
+options, remainder = getopt.gnu_getopt(sys.argv[1:], 'h')
+
+# Parse the command line arguments
+for opt, arg in options:
+		if opt=='-h':
+			print_usage()
+			sys.exit(0)
+
+def openPDF(outfile,canvas):
+	c.Print(outfile+".pdf[")
+
+def closePDF(outfile,canvas):
+	c.Print(outfile+".pdf]")
+
+def savehisto2D(histo,outfile,canvas,XaxisTitle="",YaxisTitle="",plotTitle="",stats=0,logY=0):
+	histo.SetTitle(plotTitle)
+	histo.GetXaxis().SetTitle(XaxisTitle)
+	histo.GetYaxis().SetTitle(YaxisTitle)
+	#histo.SetStats(stats)
+	histo.Draw("COLZ")
+	canvas.SetLogy(logY)
+	canvas.Print(outfile+".pdf")
+	histo.Write(plotTitle)
+
+#gStyle.SetOptStat(0)
+gStyle.SetOptStat(110011)
+c = TCanvas("c","c",800,600)
+
+outfile = remainder[0]
+infiles = []
+for i in range(len(remainder)-1):
+	infiles.append(TFile(remainder[i+1]))
+
+infile = infiles[0]
+histos = []
+infile.cd()
+for h in infile.GetListOfKeys():
+	h = h.ReadObj()
+	if(h.ClassName() != "TH1F" and h.ClassName() != "TH1D" and h.ClassName() != "TH2F" and h.ClassName() != "TH2D"): continue
+	histos.append(h)
+
+for i in range(len(infiles)-1):
+	file = infiles[i+1]
+	file.cd()
+	j = 0
+	for h in file.GetListOfKeys():
+		h = h.ReadObj()
+		if(h.ClassName() != "TH1F" and h.ClassName() != "TH1D" and h.ClassName() != "TH2F" and h.ClassName() != "TH2D"): continue
+		histos[j].Add(h)
+		j = j + 1
+
+openPDF(outfile,c)
+for i in range(len(histos)):
+	savehisto2D(histos[i],outfile,c,"","",histos[i].GetName())
+
+closePDF(outfile,c)
