f8d17455e65262919ed0da1bce6970606dd64a70
Matthew Solt <mrsolt@stanford.edu>
2019-11-16 00:26:27 -0800

    updating data MC comparisons

--- 2016Vertexing/DataMCCompare.py
+++ 2016Vertexing/DataMCCompare.py
@@ -156,7 +156,8 @@ for i in range(1,len(remainder)):
     events.Add(remainder[i])
 
 #List of cuts
-cuts = "uncVZ<5.7"
+#cuts = "uncVZ<5.7"
+cuts = "uncVZ<0.7"
 #Example list of cuts
 rootfile = TFile(outfile+".root","recreate")
 
@@ -167,7 +168,7 @@ plots.append("uncVY -1 1")
 plots.append("uncVX -3 3")
 plots.append("uncM 0 {0}".format(0.1*energy))
 plots.append("uncPX -0.1 0.1")
-plots.append("uncPY -0.1 0.1")
+plots.append("uncPY -0.1c 0.1")
 plots.append("uncPZ 0 {0}".format(1.6*energy))
 plots.append("uncP 0 {0}".format(1.6*energy))
 plots.append("uncChisq 0 20")
@@ -205,7 +206,7 @@ plots.append("(uncVZ-{0})/sqrt(uncCovZZ) -10 10".format(targZ))
 openPDF(outfile,c)
 
 #Loop over list of plots
-saveTuplePlot2D(events,"uncVZ","uncM",nBins,minVZ,maxVZ,nBins,0.0,0.1*energy,outfile,c,"uncVZ","uncM","uncM vs uncVZ",nentries,cuts=cuts)
+saveTuplePlot2D(events,"uncM","uncVZ",nBins,0.0,0.1*energy,nBins,minVZ,maxVZ,outfile,c,"uncM","uncVZ","uncVZ vs. uncM",nentries,cuts=cuts)
 for i in range(len(plots)):
 	plot = getPlot(plots[i]) #variable to be plotted
 	minX = getMinX(plots[i])
