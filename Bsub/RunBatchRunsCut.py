1ba3e611214a838b9d7668cbb23cb09f051fb52c
Matthew Solt <mrsolt@stanford.edu>
2020-01-10 15:38:35 -0800

    adding new bsub command

---
+++ Bsub/RunBatchRunsCut.py
@@ -0,0 +1,71 @@
+#!/usr/local/bin/python2.7
+
+# 
+# Submit Batch Job general script
+#
+#       author: Matt Solt
+#
+
+import argparse
+import math
+import os
+import operator
+import pprint
+import shutil
+import sys
+import subprocess
+import time
+
+def main() : 
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_list", help="List of runs to process.")
+    parser.add_argument("-l", "--logFile",   help="Output Logs.")
+    parser.add_argument("-o", "--outputFiles",   help="Output files.")
+    parser.add_argument("-f", "--inputFiles",   help="Input files.")
+    parser.add_argument("-c", "--clT",   help="cluster time shift.")
+    parser.add_argument("-W", "--time",   help="Time of job for batch.")
+
+    args = parser.parse_args()
+
+    # If a list of input files has not been specified, warn the user and 
+    # exit the application.
+    if args.input_list is None : 
+        print "A list of lcio files needs to be specified." 
+        sys.exit(2)
+
+    if args.com is None : 
+        print "A command needs to be specified." 
+        sys.exit(2)
+
+    #Time of each batch job. Default is no batch job submitted
+    bsub = ""
+    if(args.time is not None):
+        bsub = "bsub -W " + args.time + ' -R "rhel60" '
+
+    # Open the file containing the list of stdhep files to process
+    try : 
+        run_list = open(args.input_list, 'r')
+    except IOError : 
+        print "Unable to open file " + str(args.input_list)
+        sys.exit(2)
+
+    cutcommand = " python /nfs/slac/g/hps2/mrsolt/hps/HPS-CODE/ANALYSIS/vertexing/vertexCuts2016.py -c {0} -j {1} -k {3} -m {4} -n {5} -o {6} -p {7} -a {8} -b {9} "
+    for line in run_list : 
+        #run = line.strip()
+        run, uncVX, uncVXSig, uncVY, uncVYSig, uncTargProjX, uncTargProjXSig, uncTargProjY, uncTargProjYSig = line.split()
+        print "Processing run: " + str(line.strip())
+        # Command that will be submitted to the batch system
+        log = args.logFile + "_{0}.log".format(run)
+        uncVX = 0
+        cut = cutcommand.format(args.clT,uncVX,uncVXSig,uncVY,uncVYSig,uncTargProjX,uncTargProjXSig,uncTargProjY,uncTargProjYSig)
+        command = bsub + " -o " + log + cut + outputFiles.format(run) + " " + inputFiles.format(run)
+        subprocess.Popen(command, shell=True).wait() 
+        print command
+        files = ""
+        time.sleep(1)
+
+if __name__ == "__main__" : 
+    main() 
+
