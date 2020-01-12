0921be600422394524e6897a02d8bbb037b3fe76
Matthew Solt <mrsolt@stanford.edu>
2020-01-11 18:20:44 -0800

    adding tailfit code

---
+++ 2016Vertexing/TailFits.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+import sys, array,math
+import getopt
+import ROOT
+from ROOT import gROOT, TCanvas, TF1, TFile, gStyle, TFormula, TGraph, TGraphErrors, TH1D, TCutG, TH2D, gDirectory, RooDataSet, RooRealVar, RooArgSet, RooFormulaVar, RooWorkspace, RooAbsData, RooGlobalFunc, RooFit, RooAbsReal, RooArgList, gPad
+
+def frange(x, y, jump):
+	while x < y:
+		yield x
+		x += jump
+
+def print_usage():
+    print "\nUsage: {0} <output basename> <input ROOT file>".format(sys.argv[0])
+    print "Arguments: "
+    print '\t-h: this help message'
+    print "\n"
+
+options, remainder = getopt.gnu_getopt(sys.argv[1:], 'h')
+
+massVar = "uncM"
+
+for opt, arg in options:
+    if opt=='-h':
+        print_usage()
+        sys.exit(0)
+
+
+def getZCut(zcut_val=0.5,scale=1.0,zBin=0.001,minZ=-60,maxZ=60):
+    iMax = int((maxZ-minZ)/zBin)
+    for i in range(iMax):
+        z = maxZ - zBin*i
+        integral = fitfunc.Integral(z,maxZ)
+        if(integral > zcut_val/scale):
+            return z
+    print("ZCut not found!")
+    return -9999.
+
+gROOT.SetBatch(True)
+gStyle.SetOptFit(1)
+gStyle.SetOptStat(1111)
+c = TCanvas("c","c",800,600);
+c.Print(remainder[0]+".pdf[")
+outfile = TFile(remainder[0]+".root","RECREATE")
+
+inFile = TFile(remainder[1])
+events = inFile.Get("ntuple")
+
+events.Draw("uncVZ:{0}>>hnew(100,0,0.1,100,-50,50)".format(massVar),"","colz")
+c.Print(remainder[0]+".pdf")
+
+#fitfunc = TF1("fitfunc","[0]*exp( ((x-[1])<[3])*(-0.5*(x-[1])^2/[2]^2) + ((x-[1])>=[3])*(-0.5*[3]^2/[2]^2-(x-[1]-[3])/[4]))")
+#fitfunc.SetParName(0,"Amplitude")
+#fitfunc.SetParName(1,"Mean")
+#fitfunc.SetParName(2,"Sigma")
+#fitfunc.SetParName(3,"Tail Z")
+#fitfunc.SetParName(4,"Tail length")
+
+fitfunc = TF1("fitfunc","[0]*exp(((x-[1])>=[3])*(pow([3]/2.0,2.0)-[3]*(x-[1])/[2]))")#,-60,60)
+fitfunc.SetParName(0,"Amplitude")
+fitfunc.SetParName(1,"Mean")
+fitfunc.SetParName(2,"Sigma")
+fitfunc.SetParName(3,"Tail Z")
+
+massarray=array.array('d')
+zeroArr=array.array('d')
+meanarray=array.array('d')
+sigmaarray=array.array('d')
+breakzarray=array.array('d')
+#lengtharray=array.array('d')
+zcutarray=array.array('d')
+zcutscaledarray=array.array('d')
+meanErr=array.array('d')
+sigmaErr=array.array('d')
+breakzErr=array.array('d')
+#lengthErr=array.array('d')
+#zcutErr=array.array('d')
+#zcutscaledErr=array.array('d')
+
+n_massbins=50
+minmass=0.04/2.
+maxmass=0.12/2.
+
+mres_p0 = 0.005
+mres_p1 = 0
+masscut_nsigma = 2.80
+scale = 1
+
+for i in range(0,n_massbins):
+    mass = minmass+i*(maxmass-minmass)/(n_massbins-1)
+    massarray.append(mass)
+    zeroArr.append(0)
+
+    c.Clear()
+    c.Divide(1,2)
+    c.cd(1)
+    events.Draw("uncVZ:{0}>>hnew2d(100,0,0.1,100,-50,50)".format(massVar),"abs({0}-{1})<{2}/2*({3}+{4}*{0})".format(massVar,mass,masscut_nsigma,mres_p0,mres_p1),"colz")
+    c.cd(2)
+    gPad.SetLogy(1)
+    events.Draw("uncVZ>>hnew1d(200,-50,50)","abs({0}-{1})<{2}/2*({3}+{4}*{0})".format(massVar,mass,masscut_nsigma,mres_p0,mres_p1),"")
+
+    h1d = gDirectory.Get("hnew1d")
+    fit=h1d.Fit("gaus","QS")
+    peak=fit.Get().Parameter(0)
+    mean=fit.Get().Parameter(1)
+    sigma=fit.Get().Parameter(2)
+    fit=h1d.Fit("gaus","QS","",mean-3*sigma,mean+3*sigma)
+    mean=fit.Get().Parameter(1)
+    sigma=fit.Get().Parameter(2)
+    fitfunc.SetParameters(peak,mean,sigma,3*sigma,5);
+    fit=h1d.Fit(fitfunc,"LSQIM","",mean-2*sigma,mean+10*sigma)
+    meanarray.append(fit.Get().Parameter(1))
+    sigmaarray.append(fit.Get().Parameter(2))
+    breakzarray.append(fit.Get().Parameter(3))
+    #lengtharray.append(fit.Get().Parameter(4))
+    meanErr.append(fit.Get().ParError(1))
+    sigmaErr.append(fit.Get().ParError(2))
+    breakzErr.append(fit.Get().ParError(3))
+    #lengthErr.append(fit.Get().ParError(4))
+    zcut = getZCut()
+    zcut_scaled = getZCut(scale=scale)
+    zcutarray.append(zcut)
+    #zcutErr.append(0)
+    zcutscaledarray.append(zcut_scaled)
+    #zcutscaledErr.append(0)
+
+    c.Print(remainder[0]+".pdf","Title:mass_{0}".format(mass))
+
+c.Clear()
+outfile.cd()
+
+graph=TGraph(len(massarray),massarray,meanarray)
+graph=TGraphErrors(len(massarray),massarray,meanarray,zeroArr,meanErr)
+graph.Draw("A*")
+graph.SetTitle("Fitted Mean")
+graph.GetXaxis().SetTitle("mass [GeV]")
+graph.GetYaxis().SetTitle("mean [mm]")
+graph.Fit("pol3")
+graph.Write("mean")
+c.Print(remainder[0]+".pdf","Title:mean")
+
+graph=TGraph(len(massarray),massarray,sigmaarray)
+graph=TGraphErrors(len(massarray),massarray,sigmaarray,zeroArr,sigmaErr)
+graph.Draw("A*")
+graph.SetTitle("Fitted Sigma")
+graph.GetXaxis().SetTitle("mass [GeV]")
+graph.GetYaxis().SetTitle("sigma [mm]")
+graph.Fit("pol3")
+graph.Write("sigma")
+c.Print(remainder[0]+".pdf","Title:sigma")
+
+graph=TGraph(len(massarray),massarray,breakzarray)
+graph=TGraphErrors(len(massarray),massarray,breakzarray,zeroArr,breakzErr)
+graph.Draw("A*")
+graph.SetTitle("Tail Z")
+graph.GetXaxis().SetTitle("mass [GeV]")
+graph.GetYaxis().SetTitle("tail Z [mm]")
+graph.Fit("pol3")
+graph.Write("breakz")
+c.Print(remainder[0]+".pdf","Title:tailz")
+
+#graph=TGraphErrors(len(massarray),massarray,lengtharray,zeroArr,lengthErr)
+#graph.Draw("A*")
+#graph.SetTitle("Tail length")
+#graph.GetXaxis().SetTitle("mass [GeV]")
+#graph.GetYaxis().SetTitle("tail length [mm]")
+#graph.Fit("pol3")
+#graph.Write("length")
+#c.Print(remainder[0]+".pdf","Title:length")
+
+graph=TGraph(len(massarray),massarray,zcutarray)
+#graph=TGraphErrors(len(massarray),massarray,zcutarray,zeroArr,zcutErr)
+graph.Draw("A*")
+graph.SetTitle("Zcut at 1/2 Background")
+graph.GetXaxis().SetTitle("mass [GeV]")
+graph.GetYaxis().SetTitle("zcut [mm]")
+graph.Fit("pol3")
+graph.Write("zcut")
+c.Print(remainder[0]+".pdf","Title:zcut")
+
+graph=TGraph(len(massarray),massarray,zcutscaledarray)
+#graph=TGraphErrors(len(massarray),massarray,zcutscaledarray,zeroArr,zcutscaledErr)
+graph.Draw("A*")
+graph.SetTitle("Zcut Scaled x{0:.2f} at 1/2 Background".format(scale))
+graph.GetXaxis().SetTitle("mass [GeV]")
+graph.GetYaxis().SetTitle("zcut [mm]")
+graph.Fit("pol3")
+graph.Write("zcutscaled")
+c.Print(remainder[0]+".pdf","Title:zcutscaled")
+
+c.Print(remainder[0]+".pdf]")
+outfile.Write()
+outfile.Close()
+sys.exit(0)
