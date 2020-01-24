5d9304fceb82779137fd152c512b9a98c1cec039
Matthew Solt <mrsolt@stanford.edu>
2020-01-23 20:04:23 -0800

    adding Z0 and V0 plot scripts

---
+++ 2016Vertexing/V0PositionExtrapPlots.py
@@ -0,0 +1,197 @@
+import sys
+tmpargv = sys.argv
+sys.argv = []
+import getopt
+import array
+import ROOT
+from ROOT import gROOT, TFile, TTree, TChain, gDirectory, TLine, gStyle, TCanvas, TLegend, TH1F, TF1, TGraphErrors, TMultiGraph
+sys.argv = tmpargv
+
+#List arguments
+def print_usage():
+    print "\nUsage: {0} <output file base name> <input A' text file>".format(sys.argv[0])
+    print "Arguments: "
+    print '\t-h: this help message'
+    print
+
+zbin = 5.
+zTarg = -4.3
+maxZ = 80
+options, remainder = getopt.gnu_getopt(sys.argv[1:], 'h')
+
+# Parse the command line arguments
+for opt, arg in options:
+		if opt=='-h':
+			print_usage()
+			sys.exit(0)
+
+gStyle.SetOptStat(0)
+gStyle.SetOptFit(1011)
+c = TCanvas("c","c",800,600)
+
+def saveTupleFitPlot(events,inHisto,nBins,mass,minX,maxX,outfile,canvas):
+	events.Draw("{0}>>histo({1},{2},{3})".format(inHisto,nBins,minX,maxX))
+	histo = ROOT.gROOT.FindObject("histo")
+	histo.SetTitle("{0} {1:.0f} A' mass".format(inHisto,mass))
+	histo.GetXaxis().SetTitle("{0} (mm)".format(inHisto))
+	f1 = TF1("f1","gaus",histo.GetMean()-1*histo.GetRMS(),histo.GetMean()+1*histo.GetRMS())
+	histo.Fit("f1","R")
+	histo.Draw("PE")
+	canvas.Print(outfile+".pdf")
+	mean = f1.GetParameter(1)
+	sigma = f1.GetParameter(2)
+	meanerror = f1.GetParError(1)
+	sigmaerror = f1.GetParError(2)
+	del f1
+	del histo
+	return mean, sigma, meanerror, sigmaerror
+
+def saveTupleFitPlotsZ(events,inHisto,nBins,mass,minX,maxX,zbin,zTarg,maxZ,outfile,canvas):
+	events.Draw("{0}:triEndZ>>histo({1},{2},{3},{1},{4},{5})".format(inHisto,nBins,zTarg,maxZ,minX,maxX))
+	histo = ROOT.gROOT.FindObject("histo")
+	histo.SetTitle("{0} vs Truth Z".format(inHisto))
+	histo.GetXaxis().SetTitle("Truth Z (mm)")
+	histo.GetYaxis().SetTitle("{0} (mm)".format(inHisto))
+	histo.Draw("COLZ")
+	canvas.Print(outfile+".pdf")
+	z = array.array('d')
+	fittedmean = array.array('d')
+	fittedsigma = array.array('d')
+	fittedmeanerror = array.array('d')
+	fittedsigmaerror = array.array('d')
+	zErr = array.array('d')
+	nbins = int((maxZ-zTarg)/zbin)
+	for i in range(nbins):
+		zmin = zTarg + i * zbin
+		zmax = zmin + zbin
+		zErr.append(0.0)
+		z.append(zmin + zbin/2.)
+		events.Draw("{0}>>histo2({1},{2},{3})".format(inHisto,nBins,minX,maxX),"triEndZ>{0}&&triEndZ<{1}".format(zmin,zmax))
+		histo2 = ROOT.gROOT.FindObject("histo2")
+		mean, sigma, meanerror, sigmaerror = getFitZ(histo2)
+		fittedmean.append(mean)
+		fittedsigma.append(sigma)
+		fittedmeanerror.append(meanerror)
+		fittedsigmaerror.append(sigmaerror)
+		del histo2
+
+	gr_mean = TGraphErrors(len(z),z,fittedmean,zErr,fittedmeanerror)
+	gr_sigma = TGraphErrors(len(z),z,fittedsigma,zErr,fittedsigmaerror)
+
+	gr_mean.Draw("AP")
+	gr_mean.SetTitle("{0} Fitted Mean {1:.0f} MeV A'".format(plot,mass))
+	gr_mean.GetXaxis().SetTitle("Truth Z (mm)")
+	gr_mean.GetYaxis().SetTitle("Mean (mm)")
+	gr_mean.GetYaxis().SetRangeUser(-0.4,0)
+	canvas.Print(outfile+".pdf")
+	gr_sigma.Draw("AP")
+	gr_sigma.SetTitle("{0} Fitted Sigma {1:.0f} MeV A'".format(plot,mass))
+	gr_sigma.GetXaxis().SetTitle("Truth Z (mm)")
+	gr_sigma.GetYaxis().SetTitle("Sigma (mm)")
+	gr_sigma.GetYaxis().SetRangeUser(0,0.4)
+	canvas.Print(outfile+".pdf")
+	del histo
+	del gr_mean
+	del gr_sigma
+
+def getFitZ(histo):
+	f1 = TF1("f1","gaus",histo.GetMean()-1*histo.GetRMS(),histo.GetMean()+1*histo.GetRMS())
+	histo.Fit(f1,"R")
+	if(f1 != None):
+		mean = f1.GetParameter(1)
+		sigma = f1.GetParameter(2)
+		meanerror = f1.GetParError(1)
+		sigmaerror = f1.GetParError(2)
+		del f1
+		del histo
+		return mean, sigma, meanerror, sigmaerror
+	else:
+		del f1
+		del histo
+		return 0, 0, 0, 0
+	
+def openPDF(outfile,canvas):
+	c.Print(outfile+".pdf[")
+
+def closePDF(outfile,canvas):
+	c.Print(outfile+".pdf]")
+
+def getPlot(string):
+	arr = string.split(" ")
+	return arr[0]
+
+def getMin(string):
+	arr = string.split(" ")
+	if(len(arr) < 2): return ""
+	else: return float(arr[1])
+
+def getMax(string):
+	arr = string.split(" ")
+	if(len(arr) < 3): return ""
+	else: return float(arr[2])
+
+
+nBins = 50
+
+outfile = remainder[0]
+
+apfile = open(remainder[1],"r")
+apfiles = []
+events = []
+mass = array.array('d')
+
+for line in (raw.strip().split() for raw in apfile):
+	apfiles.append(TFile(line[0]))
+
+masserror = array.array('d')
+for i in range(len(apfiles)):
+	events.append(apfiles[i].Get("ntuple"))
+	events[i].Draw("triM>>dummy({0},{1},{2})".format(1000,0,1))
+	dummy = ROOT.gROOT.FindObject("dummy")
+	mass.append(dummy.GetMean()*1000)
+	masserror.append(0.0)
+	del dummy
+
+fittedmean = array.array('d')
+fittedsigma = array.array('d')
+fittedmeanerror = array.array('d')
+fittedsigmaerror = array.array('d')
+
+plots = []
+plots.append("uncVX -1 1")
+plots.append("uncVY -0.5 0.5")
+plots.append("uncTargProjX -1 1")
+plots.append("uncTargProjY -0.5 0.5")
+
+openPDF(outfile,c)
+for i in range(len(plots)):
+	for j in range(len(mass)):
+		plot = getPlot(plots[i])
+		minX = getMin(plots[i])
+		maxX = getMax(plots[i])
+		mean, sigma, meanerror, sigmaerror = saveTupleFitPlot(events[j],plot,nBins,mass[j],minX,maxX,outfile,c)
+		saveTupleFitPlotsZ(events[j],plot,nBins,mass[j],minX,maxX,zbin,zTarg,maxZ,outfile,c)
+		fittedmean.append(mean)
+		fittedsigma.append(sigma)
+		fittedmeanerror.append(meanerror)
+		fittedsigmaerror.append(sigmaerror)
+
+	gr_mean = TGraphErrors(len(mass),mass,fittedmean,masserror,fittedmeanerror)
+	gr_sigma = TGraphErrors(len(mass),mass,fittedsigma,masserror,fittedsigmaerror)
+
+	gr_mean.SetTitle("{0} Mean ".format(plot))
+	gr_mean.GetXaxis().SetTitle("Truth Mass (MeV)")
+	gr_mean.GetYaxis().SetTitle("Mean (mm)")
+	gr_mean.GetYaxis().SetRangeUser(-0.4,0)
+	gr_mean.Draw("AP")
+	c.Print(outfile+".pdf")
+
+	gr_sigma.SetTitle("{0} Sigma ".format(plot))
+	gr_sigma.GetXaxis().SetTitle("Truth Mass (MeV)")
+	gr_sigma.GetYaxis().SetTitle("Sigma (mm)")
+	gr_sigma.GetYaxis().SetRangeUser(0,0.4)
+	gr_sigma.Draw("AP")
+
+	c.Print(outfile+".pdf")
+
+closePDF(outfile,c)
---
+++ 2016Vertexing/Z0Plots.py
@@ -0,0 +1,140 @@
+import sys
+tmpargv = sys.argv
+sys.argv = []
+import getopt
+import ROOT
+from ROOT import gROOT, TFile, TTree, TChain, gDirectory, TLine, gStyle, TCanvas, TLegend, TH1F
+sys.argv = tmpargv
+
+#List arguments
+def print_usage():
+    print "\nUsage: {0} <output file base name> <input text Data file> <input text MC file> <input text A' files>".format(sys.argv[0])
+    print "Arguments: "
+    print '\t-z: target position (default -4.3 mm)'
+    print '\t-m: minimum uncVZ (default -30 mm)'
+    print '\t-n: maximum uncVZ (default 60 mm)'
+    print '\t-e: beam energy (default 2.3 GeV)'
+    print '\t-b: number of bins (default 100)'
+    print '\t-a: do not use Ap file (default use)'
+    print '\t-h: this help message'
+    print
+
+zTarg = -4.3
+ebeam = 2.3
+minVZ = -20
+maxVZ = 60
+nBins = 100
+useAp = True
+
+
+options, remainder = getopt.gnu_getopt(sys.argv[1:], 'hz:m:n:e:b:a')
+
+# Parse the command line arguments
+for opt, arg in options:
+		if opt=='-z':
+			zTarg = float(arg)
+		if opt=='-m':
+			minVZ = float(arg)
+		if opt=='-n':
+			maxVZ = float(arg)
+		if opt=='-e':
+			ebeam = float(arg)
+		if opt=='-b':
+			nBins = float(arg)
+		if opt=='-a':
+			useAp = False
+		if opt=='-h':
+			print_usage()
+			sys.exit(0)
+
+gStyle.SetOptStat(0)
+c = TCanvas("c","c",800,600)
+
+def saveTupleFitPlot(events,inHisto,nBins,mass,minX,maxX,minVZ,maxVZ,cut,outfile,canvas):
+	events.Draw("{0}:uncVZ>>histo({1},{4},{5},{1},{2},{3})".format(inHisto,nBins,minX,maxX,minVZ,maxVZ),cut)
+	histo = ROOT.gROOT.FindObject("histo")
+	histo.SetTitle("{0} vs uncVZ {1:.0f} MeV Mass Bin".format(inHisto,mass*1000))
+	histo.GetXaxis().SetTitle("uncVZ (mm)")
+	histo.GetYaxis().SetTitle("{0} (mm)".format(inHisto))
+	histo.Draw("COLZ")
+	canvas.Print(outfile+".pdf")
+
+def openPDF(outfile,canvas):
+	c.Print(outfile+".pdf[")
+
+def closePDF(outfile,canvas):
+	c.Print(outfile+".pdf]")
+
+def getPlot(string):
+	arr = string.split(" ")
+	return arr[0]
+
+def getMin(string):
+	arr = string.split(" ")
+	if(len(arr) < 2): return ""
+	else: return float(arr[1])
+
+def getMax(string):
+	arr = string.split(" ")
+	if(len(arr) < 3): return ""
+	else: return float(arr[2])
+
+outfile = remainder[0]
+
+if(not useAp):
+	inFile = TFile(remainder[1])
+	events = inFile.Get("ntuple")
+	mass = []
+	nMass = 20
+	minMass = 0.050
+	massBin = 0.005
+	for i in range(nMass):
+		m = minMass + i * massBin
+		mass.append(m)
+
+if(useAp):
+	apfile = open(remainder[1],"r")
+	apfiles = []
+	events = []
+	mass = []
+
+	for line in (raw.strip().split() for raw in apfile):
+		apfiles.append(TFile(line[0]))
+
+	for i in range(len(apfiles)):
+		events.append(apfiles[i].Get("ntuple"))
+		events[i].Draw("triM>>dummy({0},{1},{2})".format(1000,0,1))
+		dummy = ROOT.gROOT.FindObject("dummy")
+		mass.append(dummy.GetMean())
+		del dummy
+
+minZ0 = -2.0
+maxZ0 = -minZ0
+plots = []
+plots.append("eleTrkZ0 {0} {1}".format(minZ0,maxZ0))
+plots.append("posTrkZ0 {0} {1}".format(minZ0,maxZ0))
+plots.append("sqrt(uncCovZZ) {0} {1}".format(0,5))
+
+openPDF(outfile,c)
+
+if(useAp):
+	for i in range(len(plots)):
+		plot = getPlot(plots[i])
+		minimum = getMin(plots[i])
+		maximum = getMax(plots[i])
+		mass_range = 0.005
+		for j in range(len(mass)):
+			masscut = "uncM>{0}&&uncM<{1}".format(mass[j]-mass_range,mass[j]+mass_range)
+			saveTupleFitPlot(events[j],plot,nBins,mass[j],minimum,maximum,minVZ,maxVZ,masscut,outfile,c)
+
+if(not useAp):
+	for i in range(len(plots)):
+		plot = getPlot(plots[i])
+		minimum = getMin(plots[i])
+		maximum = getMax(plots[i])
+		mass_range = 0.005
+		for j in range(len(mass)):
+			masscut = "uncM>{0}&&uncM<{1}".format(mass[j]-mass_range,mass[j]+mass_range)
+			saveTupleFitPlot(events,plot,nBins,mass[j],minimum,maximum,minVZ,maxVZ,masscut,outfile,c)
+
+closePDF(outfile,c)
