9a01f0ed279ae4c366f72365d62590237fae75c4
Matthew Solt <mrsolt@stanford.edu>
2020-05-18 11:43:35 -0700

    adding z0 runs script

---
+++ 2016Vertexing/Z0RunPlots.py
@@ -0,0 +1,156 @@
+import sys
+tmpargv = sys.argv
+sys.argv = []
+import getopt
+import ROOT
+from ROOT import gROOT, TFile, TTree, TChain, gDirectory, TLine, gStyle, TCanvas, TLegend, TH1F, TLatex
+sys.argv = tmpargv
+
+#List arguments
+def print_usage():
+    print "\nUsage: {0} <output file base name> <input root file>".format(sys.argv[0])
+    print "Arguments: "
+    print '\t-z: target position (default -4.3 mm)'
+    print '\t-g: minimum uncZ0 (default 0.5 mm)'
+    print '\t-i: maximum uncZ0 (default 0.5 mm)'
+    print '\t-e: beam energy (default 2.3 GeV)'
+    print '\t-q: number of bins (default 140)'
+    print '\t-t: cluster time offset (default 56 ns)'
+    print '\t-o: uncTargProjX mean (default 0)'
+    print '\t-p: uncTargProjX sigma (default 9999)'
+    print '\t-a: uncTargProjY mean (default 0)'
+    print '\t-b: uncTargProjY sigma (default 9999)'
+    print '\t-r: is L1L2 category (default false)'
+    print '\t-y: plot label'
+    print '\t-d: use data file (default False)'
+    print '\t-h: this help message'
+    print
+
+zTarg = -4.3
+ebeam = 2.3
+minZ0 = -0.5
+maxZ0 = 0.5
+nBins = 100
+useData = False
+L1L2 = False
+clusterT = 56
+uncTargProjX = 0.
+uncTargProjXSig = 9999.
+uncTargProjY = 0.
+uncTargProjYSig = 9999.
+label = ""
+
+options, remainder = getopt.gnu_getopt(sys.argv[1:], 'hz:g:i:e:q:t:o:p:a:b:y:rd')
+
+# Parse the command line arguments
+for opt, arg in options:
+		if opt=='-z':
+			zTarg = float(arg)
+		if opt=='-g':
+			minZ0 = float(arg)
+		if opt=='-i':
+			maxZ0 = float(arg)
+		if opt=='-e':
+			ebeam = float(arg)
+		if opt=='-q':
+			nBins = float(arg)
+		if opt=='-t':
+			clusterT = float(arg)
+		if opt=='-o':
+			uncTargProjX=float(arg)
+		if opt=='-p':
+			uncTargProjXSig=float(arg)
+		if opt=='-a':
+			uncTargProjY=float(arg)
+		if opt=='-b':
+			uncTargProjYSig=float(arg)
+		if opt=='-y':
+			label=str(arg)
+		if opt=='-r':
+			L1L2 = True
+		if opt=='-d':
+			useData = True
+		if opt=='-h':
+			print_usage()
+			sys.exit(0)
+
+gStyle.SetOptStat(0)
+c = TCanvas("c","c",800,600)
+
+def openPDF(outfile,canvas):
+	c.Print(outfile+".pdf[")
+
+def closePDF(outfile,canvas):
+	c.Print(outfile+".pdf]")
+
+file = TFile(remainder[1])
+events = file.Get("ntuple")
+
+angleMC = 0.111025680707
+angleData = 0.0386557750132
+if(useData):
+	angle = angleData
+else:
+	angle = angleMC
+xProj = "(uncVX-(uncVZ-{0})*uncPX/uncPZ)".format(zTarg)
+yProj = "(uncVY-(uncVZ-{0})*uncPY/uncPZ)".format(zTarg)
+xProj_rot = "{0}*cos({2})-{1}*sin({2})".format(xProj,yProj,-angle)
+yProj_rot = "{0}*sin({2})+{1}*cos({2})".format(xProj,yProj,-angle)
+nSig = 2
+
+cuts = []
+
+c0 = 0.0
+c1 = 0.0
+c2 = 0.0
+c3 = 0.0
+
+if(useData):
+	c0 = -0.377
+	c1 = 13.79
+	c2 = -55.84
+	c3 = 84.0
+
+dz = "{0}+{1}*uncM+{2}*uncM^2+{3}*uncM^3".format(c0,c1,c2,c3)
+
+eleiso = "eleMinPositiveIso+0.5*((eleTrkZ0+{0}*elePY/eleP)*sign(elePY)-3*(eleTrkZ0Err+abs({0}*eleTrkLambdaErr)+abs(2*{0}*eleTrkLambda*eleTrkOmegaErr/eleTrkOmega)))>0".format(zTarg)
+posiso = "posMinPositiveIso+0.5*((posTrkZ0+{0}*posPY/posP)*sign(posPY)-3*(posTrkZ0Err+abs({0}*posTrkLambdaErr)+abs(2*{0}*posTrkLambda*posTrkOmegaErr/posTrkOmega)))>0".format(zTarg)
+
+isocut = "({0}&&{1})".format(eleiso,posiso)
+
+cuts.append("eleHasL1&&posHasL1&&eleHasL2&&posHasL2")
+cuts.append("sqrt((({4}-{0})/({6}*{1}))^2+(({5}-{2})/({6}*{3}))^2)<1".format(uncTargProjX,uncTargProjXSig,uncTargProjY,uncTargProjYSig,xProj_rot,yProj_rot,nSig))
+cuts.append("uncP>2.0")
+cuts.append(isocut)
+
+cut_all = ""
+
+for i in range(len(cuts)):
+	cut = cuts[i]
+	if(i == 0):
+		cut_all = cut
+	else:
+		cut_all = cut_all + "&&" + cut
+
+outfile = remainder[0]
+outfileroot = TFile(remainder[0]+".root","RECREATE")
+
+openPDF(outfile,c)
+
+events.Draw("{0}>>{1}({2},{3},{4})".format("eleTrkZ0-{0}*elePY/eleP-{1}".format(zTarg,uncTargProjY),"histo0",nBins,minZ0,maxZ0),cut_all)
+events.Draw("{0}>>{1}({2},{3},{4})".format("posTrkZ0-{0}*posPY/posP-{1}".format(zTarg,uncTargProjY),"histo1",nBins,minZ0,maxZ0),cut_all)
+histo0 = ROOT.gROOT.FindObject("histo0")
+histo1 = ROOT.gROOT.FindObject("histo1")
+
+histo0.Add(histo1)
+histo0.SetStats(0)
+histo0.SetTitle("Shifted Track Z0")
+histo0.GetXaxis().SetTitle("Track Z0 (mm)")
+histo0.Draw()
+
+outfileroot.cd()
+c.Print(outfile+".pdf")
+histo0.Write("histo")
+
+closePDF(outfile,c)
+outfileroot.Close()
