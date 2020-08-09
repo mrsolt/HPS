5bffd71f79b191d1ad893e7bfdac22a33150fb5c
Matthew Solt <matthewsolt@Matthews-MacBook-Pro.local>
2020-08-09 00:25:57 -0400

    add L2L2 to plot 2D

--- 2016Vertexing/Plot2D.py
+++ 2016Vertexing/Plot2D.py
@@ -12,15 +12,17 @@ def print_usage():
     print "\nUsage: {0} <output file base name> <input file> <label>".format(sys.argv[0])
     print '\t-z: plot zcut (default false)'
     print '\t-r: is L1L2 (default false)'
+    print '\t-t: is L2L2 (default false)'
     print '\t-s: plot shaded region (default false)'
     print '\t-h: this help message'
     print
 
 plotZcut = False
 isL1L2 = False
+isL2L2 = False
 Shaded = False
 
-options, remainder = getopt.gnu_getopt(sys.argv[1:], 'zrsh')
+options, remainder = getopt.gnu_getopt(sys.argv[1:], 'zrtsh')
 
 # Parse the command line arguments
 for opt, arg in options:
@@ -28,6 +30,8 @@ for opt, arg in options:
 			plotZcut = True
 		if opt=='-r':
 			isL1L2 = True
+        if opt=='-t':
+			isL2L2 = True
 		if opt=='-s':
 			Shaded = True
 		if opt=='-h':
@@ -75,7 +79,23 @@ if(plotZcut):
 	outfilezcut = outfile + "_zcut"
 	openPDF(outfilezcut,c)
 
-	if(not isL1L2):
+    if(isL1L2):
+		#fz_10per = TF1("fz_10per","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-133,8211,-162000,1480000,-6406000,10560000),0.05,0.150) #L1L2 10%
+		#fz_scaled = TF1("fz_scaled","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-156.8,9720,-191100,1736000,-7433000,12040000),0.05,0.150) #L1L2 scaled 100%
+		#fz_mc = TF1("fz_mc","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(25.23,47.14,-2987,12370,0,0),0.05,0.150) #L1L2 MC
+		#fz_10per = TF1("fz_10per","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-162,9927,-2.028e5,1.952e6,-9.05e6,1.627e7),0.05,0.150) #L1L2 10%
+		#fz_scaled = TF1("fz_scaled","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-199.6,1.224e4,-2.501e5,2.411e6,-1.117e7,2.001e7),0.05,0.150) #L1L2 scaled 100%
+		#fz_mc = TF1("fz_mc","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(15.5,146.2,5740,-1.857e5,1.49e6,-3.796e6),0.05,0.150) #L1L2 MC
+		fz_10per = TF1("fz_10per","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-164.9,1.012e4,-2.087e5,2.039e6,-9.614e6,1.761e7),0.05,0.150) #L1L2 10%
+		fz_scaled = TF1("fz_scaled","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-205.6,1.258e4,-2.595e5,2.538e6,-1.197e7,2.19e7),0.05,0.150) #L1L2 scaled 100%
+		fz_mc = TF1("fz_mc","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(74.12,-2922,7.02e4,-8.567e5,4.936e6,-1.075e7),0.05,0.150) #L1L2 MC
+
+    if(isL2L2):
+		fz_10per = TF1("fz_10per","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-261.4,1.608e4,-3.09e5,2.571e6,-8.816e6,7.119e6),0.05,0.150) #L1L2 10%
+		fz_scaled = TF1("fz_scaled","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-285.2,1.681e4,-2.91e5,1.909e6,-2.401e6,-1.328e7),0.05,0.150) #L1L2 scaled 100%
+		fz_mc = TF1("fz_mc","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(74.12,-2922,7.02e4,-8.567e5,4.936e6,-1.075e7),0.05,0.150) #L1L2 MC
+
+    else:
 		#fz_10per = TF1("fz_10per","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-2.308,1227,-29030,285300,-1296000,2229000),0.05,0.150) #L1L1 10%
 		#fz_scaled = TF1("fz_scaled","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(1.628,1301,-31950,318900,-1456000,2497000),0.05,0.150) #L1L1 scaled 100%
 		#fz_mc = TF1("fz_mc","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(22.23,48.63,-5150,49760,-169900,141700),0.05,0.150) #L1L1 MC
@@ -86,16 +106,6 @@ if(plotZcut):
 		fz_10per= TF1("fz_10per","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-0.4738,1551,-3.835e4,3.930e5,-1.865e6,3.373e6),0.05,0.150) #L1L1 100%
 		fz_scaled = TF1("fz_scaled","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(26.85,-124.3,593.6,-3.954e4,4.451e5,-1.393e6),0.05,0.150) #L1L1 scaled 100%
 		fz_mc = TF1("fz_mc","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(23.55,152.9,-9489,1.091e5,-5.19e5,9.013e5),0.05,0.150) #L1L1 MC
-	else:
-		#fz_10per = TF1("fz_10per","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-133,8211,-162000,1480000,-6406000,10560000),0.05,0.150) #L1L2 10%
-		#fz_scaled = TF1("fz_scaled","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-156.8,9720,-191100,1736000,-7433000,12040000),0.05,0.150) #L1L2 scaled 100%
-		#fz_mc = TF1("fz_mc","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(25.23,47.14,-2987,12370,0,0),0.05,0.150) #L1L2 MC
-		#fz_10per = TF1("fz_10per","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-162,9927,-2.028e5,1.952e6,-9.05e6,1.627e7),0.05,0.150) #L1L2 10%
-		#fz_scaled = TF1("fz_scaled","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-199.6,1.224e4,-2.501e5,2.411e6,-1.117e7,2.001e7),0.05,0.150) #L1L2 scaled 100%
-		#fz_mc = TF1("fz_mc","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(15.5,146.2,5740,-1.857e5,1.49e6,-3.796e6),0.05,0.150) #L1L2 MC
-		fz_10per = TF1("fz_10per","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-164.9,1.012e4,-2.087e5,2.039e6,-9.614e6,1.761e7),0.05,0.150) #L1L2 10%
-		fz_scaled = TF1("fz_scaled","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(-205.6,1.258e4,-2.595e5,2.538e6,-1.197e7,2.19e7),0.05,0.150) #L1L2 scaled 100%
-		fz_mc = TF1("fz_mc","{0}+{1}*x+{2}*x^2+{3}*x^3+{4}*x^4+{5}*x^5".format(74.12,-2922,7.02e4,-8.567e5,4.936e6,-1.075e7),0.05,0.150) #L1L2 MC
 
 	histo.SetStats(0)
 	histo.Draw("COLZ")
